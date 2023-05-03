5800c86ed2b4c825374b6e334034bbf293ba00d4
Kevin <kevin.paez1010@gmail.com>
2023-05-03 16:49:20 -0600

    Validate 1 to 10 exercise

---
+++ C-Sharp-Exercises/01ValidateNum/ValidateNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Exercises._01ValidateNum
+{
+    public class ValidateNum
+    {
+
+        public int ValideType(string a)
+        {
+            int number;
+            if(int.TryParse(a, out number))
+            {
+                return number;
+            }
+            number = 0;
+            return number;
+        }
+
+        public string ValideNumber(int a)
+        {
+            if(a > 0 && a <= 10)
+            {
+                return "Valid";
+            }
+            return "Invalid";
+        }
+    }
+}
--- C-Sharp-Exercises/Program.cs
+++ C-Sharp-Exercises/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace C_Sharp_Exercises
+﻿
+
+using C_Sharp_Exercises._01ValidateNum;
+
+namespace C_Sharp_Exercises
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* First Exercise */
+            string test;
+            Console.WriteLine("Write a number between 1 to 10");
+
+            test = Console.ReadLine();
+            
+            ValidateNum validateNum = new ValidateNum();
+            int number = validateNum.ValideType(test);
+            if (number != 0)
+            {
+                Console.WriteLine(validateNum.ValideNumber(number));
+
+            }else
+            {
+                Console.WriteLine("This is not a number");
+            }
         }
     }
 }
